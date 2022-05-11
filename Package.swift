f6671a5fba4edeba571bfe926c3186a079ba484c
Helbert Emilio Gomes <helbertgs@gmail.com>
2022-05-11 20:50:06 -0300

    chore: update swiftlint file

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -32,7 +32,8 @@ let package = Package(
     targets: [
         .target(
             name: "OpenSwiftUI",
-            dependencies: [ "OpenCombine" ]),
+            dependencies: [ "OpenCombine" ],
+            plugins: [ ]),
         .testTarget(
             name: "OpenSwiftUITests",
             dependencies: ["OpenSwiftUI", "Nimble", "Quick"]),
