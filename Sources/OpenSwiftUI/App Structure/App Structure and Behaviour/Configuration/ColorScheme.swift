ee79b611755710c5daf4c3f8a30064574efa88bd
Helbert Emilio Gomes <helbertgs@gmail.com>
2022-05-12 12:16:44 -0300

    chore: update and comment ColorScheme

--- Sources/OpenSwiftUI/App Structure/App Structure and Behaviour/Configuration/ColorScheme.swift
+++ Sources/OpenSwiftUI/App Structure/App Structure and Behaviour/Configuration/ColorScheme.swift
@@ -8,34 +8,57 @@ import Swift
 @frozen public enum ColorScheme: CaseIterable, Equatable, Hashable {
 
     // MARK: - Case(s).
+
     /// Dark mode.
     case dark
 
     /// Light mode
     case light
-}
 
-#if os(iOS) && canImport(UIKit)
+    // MARK: - Type Alias.
 
-import UIKit
+    /// A type that can represent a collection of all values of this type.
+    public typealias AllCases = [ColorScheme]
 
-extension ColorScheme {
+    // MARK: - Static Property(ies).
 
-    // MARK: - Constructor(s).
+    /// A collection of all values of this type.
+    public static var allCases: [ColorScheme] = [ .dark, .light ]
 
-    /// Creates a color scheme from its user interface style equivalent.
-    @inlinable public init?(_ userInterfaceStyle: UIUserInterfaceStyle) {
-        switch userInterfaceStyle {
-            case .dark:
-                self = .dark
-            case .light:
-                self = .light
-            case .unspecified:
-                self = .light
-            @unknown default:
-                self = .light
+    // MARK: - Hashable
+
+    /// Hashes the essential components of this value by feeding them into the
+    /// given hasher.
+    ///
+    /// Implement this method to conform to the `Hashable` protocol. The
+    /// components used for hashing must be the same as the components compared
+    /// in your type's `==` operator implementation. Call `hasher.combine(_:)`
+    /// with each of these components.
+    ///
+    /// - Important: Never call `finalize()` on `hasher`. Doing so may become a
+    ///   compile-time error in the future.
+    ///
+    /// - Parameter hasher: The hasher to use when combining the components
+    ///   of this instance.
+    @inlinable public func hash(into hasher: inout Hasher) {
+        hasher.combine(self)
+    }
+
+    // MARK: - Equatable.
+
+    /// Returns a Boolean value indicating whether two values are equal.
+    ///
+    /// Equality is the inverse of inequality. For any values `a` and `b`,
+    /// `a == b` implies that `a != b` is `false`.
+    ///
+    /// - Parameters:
+    ///   - lhs: A value to compare.
+    ///   - rhs: Another value to compare.
+    @inlinable public static func == (lhs: ColorScheme, rhs: ColorScheme) -> Bool {
+        switch (lhs, rhs) {
+            case (.dark, .dark): return true
+            case (.light, .light): return true
+            default: return false
         }
     }
 }
-
-#endif
---
+++ Sources/OpenSwiftUI/User Interface Elements/Drawing and Graphics/Geometric Constructs/Namespace.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Helbert Gomes on 12/05/22.
+//
+
+import Foundation
