00f4ab6b16d568448bef29136b54530a0b1be6df
Helbert Emilio Gomes <helbertgs@gmail.com>
2022-05-11 22:22:05 -0300

    chore: update spm version

--- Package@swift-5.5.swift
+++ Package@swift-5.5.swift
@@ -1,11 +1,10 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let supportedPlatforms: [Platform] = [
     .macOS,
-    .macCatalyst,
     .iOS,
     .watchOS,
     .tvOS,
