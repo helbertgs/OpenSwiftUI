cc06f0f8e6b9948b562501ab80d0e132cf4494d9
Helbert Emilio Gomes <helbertgs@gmail.com>
2022-06-15 19:06:46 -0300

    chore: some improvements

--- Sources/OpenSwiftUI/App Structure/Scenes/Life Cycle/_SceneInputs.swift
+++ Sources/OpenSwiftUI/App Structure/Scenes/Life Cycle/_SceneInputs.swift
@@ -1,3 +1,22 @@
 import Swift
 
-public struct _SceneInputs { }
+@dynamicMemberLookup
+@frozen public struct _SceneInputs {
+
+    // MARK: - Property(ies).
+
+    @usableFromInline var props: [String: Any]
+
+    // MARK: - Constructor(s).
+
+    @inlinable internal init(props: [String : Any] = [:]) {
+        self.props = props
+    }
+
+    // MARK: - Subscript(s).
+
+    @inlinable internal subscript<U>(dynamicMember member: String) -> U {
+        get { props[member] as! U }
+        set { props[member] = newValue }
+    }
+}
