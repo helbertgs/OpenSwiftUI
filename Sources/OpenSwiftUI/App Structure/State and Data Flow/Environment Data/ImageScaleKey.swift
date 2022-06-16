58b497de2d9287dc2e84f764b5af07d52c7d2fb2
Helbert Emilio Gomes <helbertgs@gmail.com>
2022-06-16 02:43:36 -0300

    fix: setup macOS min version

--- Sources/OpenSwiftUI/App Structure/State and Data Flow/Environment Data/ImageScaleKey.swift
+++ Sources/OpenSwiftUI/App Structure/State and Data Flow/Environment Data/ImageScaleKey.swift
@@ -1,11 +1,13 @@
 import Swift
 
+@available(macOS 11.0, *)
 public struct ImageScaleKey : EnvironmentKey {
     public static var defaultValue: Image.Scale {
         .medium
     }
 }
 
+@available(macOS 11.0, *)
 extension EnvironmentValues {
     /// The image scale for this environment.
     public var imageScale: Image.Scale {
