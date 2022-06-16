cfbd0ad4bbe2025c1909ce303912b2132d498c9a
Helbert Emilio Gomes <helbertgs@gmail.com>
2022-06-16 02:14:49 -0300

    fix: swiftlint warnings

--- Sources/OpenSwiftUI/User Interface Elements/View Fundamentals/View Modifiers/_ValueActionModifier.swift
+++ Sources/OpenSwiftUI/User Interface Elements/View Fundamentals/View Modifiers/_ValueActionModifier.swift
@@ -24,7 +24,6 @@ import Swift
 //    public static func _makeViewList(modifier: _GraphValue<_ValueActionModifier<Value>>, inputs: _ViewListInputs, body: @escaping (_Graph, _ViewListInputs) -> _ViewListOutputs) -> _ViewListOutputs
 }
 
-
 @available(iOS 13.0, macOS 11.0, tvOS 14.0, watchOS 7.0, *)
 extension _ValueActionModifier : _SceneModifier {
     public static func _makeScene(modifier: _GraphValue<_ValueActionModifier<Value>>, inputs: _SceneInputs, body: @escaping (_Graph, _SceneInputs) -> _SceneOutputs) -> _SceneOutputs {
