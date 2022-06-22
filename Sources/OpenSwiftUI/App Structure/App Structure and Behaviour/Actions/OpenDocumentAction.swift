73a61eedb52bf24c1e1f1e1af31521426b46b89d
Helbert Emilio Gomes <helbertgs@gmail.com>
2022-06-22 01:52:42 -0300

    fix: fix windows limitations

--- Sources/OpenSwiftUI/App Structure/App Structure and Behaviour/Actions/OpenDocumentAction.swift
+++ Sources/OpenSwiftUI/App Structure/App Structure and Behaviour/Actions/OpenDocumentAction.swift
@@ -5,6 +5,7 @@ import Swift
 @available(iOS, unavailable)
 @available(tvOS, unavailable)
 @available(watchOS, unavailable)
+@available(Windows, unavailable)
 public struct OpenDocumentAction {
 
     /// Opens the document at the specified file URL, if it a type that's
@@ -16,6 +17,50 @@ public struct OpenDocumentAction {
     ///
     /// - Parameter url: A file URL which points to an existing document.
     public func callAsFunction(at url: URL) async throws {
+        fatalError()
+    }
+}
+
+@available(macOS 13.0, *)
+@available(iOS, unavailable)
+@available(tvOS, unavailable)
+@available(watchOS, unavailable)
+@available(Windows, unavailable)
+extension EnvironmentValues {
+    /// An action that presents an existing document.
+    ///
+    /// In order to present an existing document, your ``App`` must define a
+    /// ``DocumentGroup`` which handles the content type of the specified file.
+    ///
+    /// Use the ``EnvironmentValues/openDocument`` environment value to get the
+    /// instance of this structure for a given ``Environment``. Then call the
+    /// instance to present an existing document. You call the instance directly
+    /// because it defines a ``OpenDocumentAction/callAsFunction(at:)``
+    /// method that Swift calls when you call the instance.
+    ///
+    /// For example, you can define a list of recent documents to open:
+    ///
+    ///     struct RecentDocuments: View {
+    ///         var documentURLs: [URL]
+    ///         @Environment(\.openDocument) private var openDocument
+    ///
+    ///         var body: some View {
+    ///             VStack {
+    ///                 ForEach(documentURLs) { url in
+    ///                     Button(url.deletingPathExtension().lastPathComponent)
+    ///                     {
+    ///                         Task {
+    ///                            do {
+    ///                                try openDocument(at: url)
+    ///                            } catch {
+    ///                                // Handle error
+    ///                            }
+    ///                     }
+    ///                 }
+    ///             }
+    ///         }
+    ///     }
+    public var openDocument: OpenDocumentAction {
         fatalError()
     }
 }
