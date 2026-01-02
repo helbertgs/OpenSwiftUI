b21ae4e8c0f9c46b4817aeebf2ba632d4bff9c59
Mariana Rios Silveira Carvalho <marianariossc@gmail.com>
2026-01-01 19:01:50 -0500

    fix: remove AppKit references

--- Sources/OpenSwiftUISample/MyApp.swift
+++ Sources/OpenSwiftUISample/MyApp.swift
@@ -1,27 +1,10 @@
-import AppKit
 import OpenSwiftUI
-
-class MyAppDelegate: NSObject, NSApplicationDelegate {
-    var window: NSWindow?
-}
 
 @main
 struct MyApp : App {
-
-    @NSApplicationDelegateAdaptor private var delegate: MyAppDelegate
-
     var body: some Scene {
         WindowGroup("Sample") {
-            VStack {
-                EmptyView()
-
-                EmptyView()
-                    .onAppear {
-                        print("Hello, World")
-                    }
-            }
+            EmptyView()
         }
-        .defaultAppStorage(UserDefaults.standard)
-        .defaultSize(.init(width: 800, height: 600))
     }
 }
