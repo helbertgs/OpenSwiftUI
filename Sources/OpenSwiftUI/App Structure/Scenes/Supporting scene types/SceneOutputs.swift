a001e12a28889da22c82c1ab015a3c92e9552e81
Helbert Gomes <helbert.gomes@justeattakeaway.com>
2026-01-06 00:06:06 -0500

    chore: update file directory

--- Sources/OpenSwiftUI/App Structure/Scenes/Supporting scene types/SceneOutputs.swift
+++ Sources/OpenSwiftUI/App Structure/Scenes/Supporting scene types/SceneOutputs.swift
@@ -1,7 +1,11 @@
-import Swift
+import OpenSpatial
 
-@frozen public struct _SceneOutputs {
+/// The outputs of a scene.
+public struct _SceneOutputs {
 
+    // MARK: - Checking characteristics
+
+    /// The type of the scene.
     var type: Any.Type? = nil
 
     /// The scene that this output represents.
@@ -18,4 +22,27 @@ import Swift
 
     /// The content view of the scene, if applicable.
     var content: _ViewOutputs? = nil
+
+    /// The title of the scene.
+    var title: String? = nil
+
+    /// The size of the scene.
+    var size: Size3D = .one
+
+    /// The position of the scene.
+    var position: UnitPoint = .center
+
+    /// The graphics context to render the scene into.
+    var graphicsContext: GraphicsContext?
+
+    // MARK: - Rendering the scene
+
+    /// Renders the scene into the given context.
+    ///
+    /// - Parameter context: The context to render the scene into.
+    mutating func render() {
+        if graphicsContext == nil {
+            graphicsContext = GraphicsContext()
+        }
+    }
 }
