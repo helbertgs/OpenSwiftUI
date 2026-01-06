16303dc236917230e9d1637788369a9209abb961
Helbert Gomes <helbert.gomes@justeattakeaway.com>
2026-01-05 19:11:13 -0500

    chore: improve DismissSearchAction struct and supporting types

--- Sources/OpenSwiftUI/App Structure/Search/Detecting, activating, and dismissing search/DismissSearchAction.swift
+++ Sources/OpenSwiftUI/App Structure/Search/Detecting, activating, and dismissing search/DismissSearchAction.swift
@@ -1,4 +1,12 @@
-import Swift
+//
+// DismissSearchAction.swift
+// OpenSwiftUI
+//
+// Created by Helbert Gomes on Oct 11, 2023.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+// SPDX-License-Identifier: MIT
+
+import Foundation
 
 /// An action that can end a search interaction.
 ///
@@ -91,11 +99,15 @@ public struct DismissSearchAction {
 
     // MARK: - Property(ies).
 
-    var onDismiss: (() -> Void)?
+    /// The action to perform when the search is dismissed.
+    package let onDismiss: (() -> Void)?
 
     // MARK: - Constructor(s).
 
-    public init(onDismiss: (() -> Void)? = nil) {
+    /// Creates a dismiss search action with the given action.
+    ///
+    /// - Parameter onDismiss: The action to perform when the search is dismissed.
+    package init(onDismiss: (() -> Void)? = nil) {
         self.onDismiss = onDismiss
     }
 
@@ -127,49 +139,101 @@ public struct DismissSearchAction {
 }
 
 extension EnvironmentValues {
-
-    /// A Boolean value that indicates when the user is searching.
+    /// An action that ends the current search interaction.
     ///
-    /// You can read this value like any of the other ``EnvironmentValues``,
-    /// by creating a property with the ``Environment`` property wrapper:
+    /// Use this environment value to get the ``DismissSearchAction`` instance
+    /// for the current ``Environment``. Then call the instance to dismiss
+    /// the current search interaction. You call the instance directly because
+    /// it defines a ``DismissSearchAction/callAsFunction()`` method that Swift
+    /// calls when you call the instance.
     ///
-    ///     @Environment(\.isSearching) private var isSearching
+    /// When you dismiss search, OpenSwiftUI:
     ///
-    /// Get the value to find out when the user interacts with a search
-    /// field that's produced by one of the searchable modifiers, like
-    /// ``View/searchable(text:placement:prompt:)-18a8f``:
+    /// * Sets ``EnvironmentValues/isSearching`` to `false`.
+    /// * Clears any text from the search field.
+    /// * Removes focus from the search field.
     ///
-    ///     struct SearchingExample: View {
+    /// > Note: Calling this instance has no effect if the user isn't
+    /// interacting with a search field.
+    ///
+    /// Use this action to dismiss a search operation based on
+    /// another user interaction. For example, consider a searchable
+    /// view with a ``Button`` that presents more information about the first
+    /// matching item from a collection:
+    ///
+    ///     struct ContentView: View {
     ///         @State private var text = ""
     ///
     ///         var body: some View {
     ///             NavigationView {
-    ///                 SearchedView()
+    ///                 SearchResults(searchText: text)
     ///                     .searchable(text: $text)
     ///             }
     ///         }
     ///     }
     ///
-    ///     struct SearchedView: View {
-    ///         @Environment(\.isSearching) private var isSearching
+    ///     private struct SearchResults: View {
+    ///         let searchText: String
+    ///
+    ///         let items = ["a", "b", "c"]
+    ///         var filteredItems: [String] { items.filter { $0 == searchText.lowercased() } }
+    ///
+    ///         @State private var isPresented = false
     ///
     ///         var body: some View {
-    ///             Text(isSearching ? "Searching!" : "Not searching.")
+    ///             if let item = filteredItems.first {
+    ///                 Button("Details about \(item)") {
+    ///                     isPresented = true
+    ///                 }
+    ///                 .sheet(isPresented: $isPresented) {
+    ///                     NavigationView {
+    ///                         DetailView(item: item)
+    ///                     }
+    ///                 }
+    ///             }
     ///         }
     ///     }
     ///
-    /// When the user first taps or clicks in a search field, the
-    /// `isSearching` property becomes `true`. When the user cancels the
-    /// search operation, the property becomes `false`. To programmatically
-    /// set the value to `false` and dismiss the search operation, use
-    /// ``EnvironmentValues/dismissSearch``.
-    public var isSearching: Bool {
-        fatalError("")
+    /// The button becomes visible only after the user enters search text
+    /// that produces a match. When the user taps the button, OpenSwiftUI shows
+    /// a sheet that provides more information about the item, including
+    /// an Add button for adding the item to a stored list of items:
+    ///
+    ///     private struct DetailView: View {
+    ///         let item: String
+    ///
+    ///         @Environment(\.dismiss) private var dismiss
+    ///         @Environment(\.dismissSearch) private var dismissSearch
+    ///
+    ///         var body: some View {
+    ///             Text("Information about \(item).")
+    ///                 .toolbar {
+    ///                     Button("Add") {
+    ///                         // Store the item here...
+    ///
+    ///                         dismiss()
+    ///                         dismissSearch()
+    ///                     }
+    ///                 }
+    ///         }
+    ///     }
+    ///
+    /// The user can dismiss the sheet by dragging it down, effectively
+    /// canceling the operation, leaving the in-progress search interaction
+    /// intact. Alternatively, the user can tap the Add button to store the
+    /// item. Because the user is likely to be done with both the detail view
+    /// and the search interaction at this point, the button's closure also
+    /// uses the ``EnvironmentValues/dismiss`` property to dismiss the sheet,
+    /// and the `dismissSearch` property to reset the search field.
+    public var dismissSearch: DismissSearchAction {
+        get { self[DismissSearchActionKey.self] }
     }
 }
 
-extension DismissSearchAction : Equatable {
-    public static func == (_ lhs: DismissSearchAction, _ rhs: DismissSearchAction) -> Bool {
-        ObjectIdentifier(type(of: lhs.self)) == ObjectIdentifier(type(of: rhs.self))
+struct DismissSearchActionKey : EnvironmentKey {
+    
+    /// The default value of the dismiss search action.
+    static var defaultValue: DismissSearchAction {
+        .init()
     }
 }
