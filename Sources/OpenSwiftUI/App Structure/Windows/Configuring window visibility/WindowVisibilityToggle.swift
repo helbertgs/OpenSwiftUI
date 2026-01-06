8f348ab7f0122c9af8413249273d2becbfe24571
Helbert Gomes <helbert.gomes@justeattakeaway.com>
2026-01-05 19:17:31 -0500

    chore: update comments

--- Sources/OpenSwiftUI/App Structure/Windows/Configuring window visibility/WindowVisibilityToggle.swift
+++ Sources/OpenSwiftUI/App Structure/Windows/Configuring window visibility/WindowVisibilityToggle.swift
@@ -77,7 +77,7 @@ public struct WindowVisibilityToggle<Label> : View where Label : View {
     ///
     /// When you implement a custom view, you must implement a computed
     /// `body` property to provide the content for your view. Return a view
-    /// that's composed of built-in views that SwiftUI provides, plus other
+    /// that's composed of built-in views that OpenSwiftUI provides, plus other
     /// composite views that you've already defined:
     ///
     ///     struct MyView: View {
